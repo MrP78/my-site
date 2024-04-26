4673997110917372f9d22275a4bf14f1f08a8c99
Patrick Butterworth <pbutterworth@sligro.nl>
2024-04-26 12:01:48 +0200

    Setup

---
+++ app/(components)/(pages)/person/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Cards from "../../features/cards";
+
+const page = () => {
+  return (
+    <>
+      <div className="heading-1 text-center">Person page</div>
+      <Cards />
+    </>
+  );
+};
+
+export default page;
