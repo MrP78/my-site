1a4f9da57fb39c9f30a698ec8e2d76e83fceea70
Patrick Butterworth <pbutterworth@sligro.nl>
2024-04-26 10:44:24 +0200

    Setting up styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { Caveat } from "next/font/google";
+import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const caveat = Caveat({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={caveat.className}>{children}</body>
     </html>
   );
 }
