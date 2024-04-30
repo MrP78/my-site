7da768f97d517815c3873babfc7725d4b984de7f
Patrick Butterworth <pbutterworth@sligro.nl>
2024-04-30 10:57:04 +0200

    wrapped suspense

--- app/(components)/(pages)/article/page.tsx
+++ app/(components)/(pages)/article/page.tsx
@@ -2,12 +2,15 @@
 
 import { Article } from "@/app/lib/definitions";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 const Page = () => {
   const searchParams = useSearchParams();
   const title = searchParams.get("title");
   return (
     <>
-      <h1 className="heading-1 text-center">{title}</h1>
+      <Suspense>
+        <h1 className="heading-1 text-center">{title}</h1>
+      </Suspense>
       {/* <p>{params.text}</p> */}
     </>
   );
